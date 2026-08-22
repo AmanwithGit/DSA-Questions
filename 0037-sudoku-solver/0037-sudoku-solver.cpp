7d18e3c2cbaadcca934f4369bf06d11928a0aebd
Aman Kumar <122218266+AmanwithGit@users.noreply.github.com>
2026-08-22 23:43:24 +0530

    Time: 388 ms (30.22%), Space: 8.9 MB (47.75%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,50 @@
+// LeetCode 37
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+
+                if (board[row][col] != '.')
+                    continue;
+
+                for (char num = '1'; num <= '9'; num++) {
+                    if (isValid(board, row, col, num)) {
+                        board[row][col] = num;
+
+                        if (solve(board))
+                            return true;
+
+                        board[row][col] = '.';
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isValid(vector<vector<char>>& board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num)
+                return false;
+
+            if (board[i][col] == num)
+                return false;
+
+            int r = (row / 3) * 3 + i / 3;
+            int c = (col / 3) * 3 + i % 3;
+
+            if (board[r][c] == num)
+                return false;
+        }
+
+        return true;
+    }
+};
