2ebe18c4e694e0532608269396fe502a04bf7046
Aman Kumar <122218266+AmanwithGit@users.noreply.github.com>
2026-08-25 22:49:26 +0530

    Time: 2 ms (73.16%), Space: 11.1 MB (25.31%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,39 @@
+// LeetCode 51
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+        vector<bool> col(n, false);
+        vector<bool> diag1(2 * n - 1, false);
+        vector<bool> diag2(2 * n - 1, false);
+
+        function<void(int)> backtrack = [&](int row) {
+            if (row == n) {
+                ans.push_back(board);
+                return;
+            }
+
+            for (int c = 0; c < n; c++) {
+                if (col[c] || diag1[row - c + n - 1] ||
+                    diag2[row + c])
+                    continue;
+
+                board[row][c] = 'Q';
+                col[c] = true;
+                diag1[row - c + n - 1] = true;
+                diag2[row + c] = true;
+
+                backtrack(row + 1);
+
+                board[row][c] = '.';
+                col[c] = false;
+                diag1[row - c + n - 1] = false;
+                diag2[row + c] = false;
+            }
+        };
+
+        backtrack(0);
+        return ans;
+    }
+};
