2ab646abd367e4d7a12150a48fe8aa391ecb653c
Aman Kumar <122218266+AmanwithGit@users.noreply.github.com>
2026-08-26 23:31:18 +0530

    Time: 0 ms (100%), Space: 8.9 MB (37.46%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,27 @@
+// LeetCode 60
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> nums;
+        vector<int> fact(n + 1, 1);
+
+        for (int i = 1; i <= n; i++) {
+            nums.push_back(i);
+            fact[i] = fact[i - 1] * i;
+        }
+
+        k--;
+
+        string ans;
+
+        for (int i = n; i >= 1; i--) {
+            int index = k / fact[i - 1];
+            k %= fact[i - 1];
+
+            ans += to_string(nums[index]);
+            nums.erase(nums.begin() + index);
+        }
+
+        return ans;
+    }
+};
