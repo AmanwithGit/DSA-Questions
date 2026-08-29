0992af0a90db07b0383dc4686b70b764d20486ab
Aman Kumar <122218266+AmanwithGit@users.noreply.github.com>
2026-08-29 23:56:14 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.1%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,12 @@
+// LeetCode 136
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for (int num : nums)
+            ans ^= num;
+
+        return ans;
+    }
+};
